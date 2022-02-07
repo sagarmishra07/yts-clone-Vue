dcec5e39becc6ef3241f9881d8eef97b220589fb
sagar mishra <sagarmishraswor@gmail.com>
2022-02-07 15:25:43 +0545

    Added API

---
+++ src/components/containers/SimilarMovies.vue
@@ -0,0 +1,32 @@
+<template>
+  <section>
+    <h1>Similar Movies</h1>
+    <div v-for="(singlemovie, key) in SimilarMovies" :key="key">
+      <Movie :SingleMovie="singlemovie" />
+    </div>
+  </section>
+</template>
+<script>
+import Movie from "../common/Movie.vue";
+export default {
+  name: "SimilarMovies",
+  props: {
+    id: {
+      type: Number,
+    },
+    components: {
+      Movie,
+    },
+  },
+  created() {
+    this.$store.dispatch("fetchSimilarMovies", this.id);
+  },
+  computed: {
+    SimilarMovies() {
+      return this.$store.getters.similarMovies;
+    },
+  },
+};
+</script>
+<style>
+</style>
